2e42e0f18cbcb3019b057b7501dd1032f3eb7940
REDX0022 <ceranic.boris2@gmail.com>
2025-05-19 22:40:25 +0200

    added cleanup of backups so no file clutter

--- sim.tcl
+++ sim.tcl
@@ -1,3 +1,21 @@
+
+# should clean the uselss files
+puts "Cleaning Vivado backup and simulation files..."
+
+foreach f [glob -nocomplain \
+    *.str \
+    *.wdb \
+    *.backup.jou \
+    *.backup.log \
+    *.xpr.backup \
+    xsim.ini \
+    xsim.dir \
+] {
+    puts "Deleting $f"
+    file delete -force $f
+}
+
+
 # simulate.tcl — TCL script to run behavioral simulation
 open_project BoriSoC.xpr
 
